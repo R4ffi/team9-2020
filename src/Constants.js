def394d6679afac84b67f905e907658240d6d503
Hari Remo <remo.hari@isolutions.ch>
2020-10-09 20:02:58 +0200

    Add relative size

--- src/Constants.js
+++ src/Constants.js
@@ -3,4 +3,13 @@ const Constants = {
     MAX_HEIGHT: window.screen.height,
     SPEED: 7
 }
+
+export function GetAbsolutWidthPosition(numberInPercent){
+    return window.screen.width / 100 * numberInPercent;
+}
+
+export function GetAbsolutHeightPosition(numberInPercent){
+    return window.screen.height / 100 * numberInPercent;
+}
+
 export default Constants;
