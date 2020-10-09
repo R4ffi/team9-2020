1754d1382cac5e9f150ee91dbbfb7ca5e63f5f4e
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2020-10-09 17:20:22 +0200

    Remove unused ctors

--- src/Entities/floor.js
+++ src/Entities/floor.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
-import Constants from "../Constants";
 
 export default class Floor extends Component {
-    constructor(props) {
-        super(props);
-    }
+  render() {
+    const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
+    const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
+    const x = this.props.body.position.x - width / 2;
+    const y = this.props.body.position.y - height / 2;
 
-    render() {
-        const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
-        const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
-        const x = this.props.body.position.x - width / 2;
-        const y = this.props.body.position.y - height / 2;
-
-        return ( <
-            div style = {
-                { position: "absolute", width: width, height: height, backgroundColor: "red", left: x, top: y }
-            }
-            />
-            // <Animated.Image
-        );
-    }
+    return (
+      <div
+        style={{
+          position: "absolute",
+          width: width,
+          height: height,
+          backgroundColor: "red",
+          left: x,
+          top: y,
+        }}
+      />
+      // <Animated.Image
+    );
+  }
 }
