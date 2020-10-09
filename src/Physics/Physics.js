fe2edf555296b0bcfb82d636c46dd1e3ea2020fd
gabriel.frischknecht <gabriel.frischknecht@hotmail.ch>
2020-10-09 19:15:04 +0200

    fixed left right click

--- src/Physics/Physics.js
+++ src/Physics/Physics.js
@@ -7,8 +7,13 @@ const Physics = (entities, { touches, time, input }) => {
     const { payload } = input.find(x => x.name === "onMouseDown") || {};
     if (payload) {
         const player = entities["player1"];
-        let movment = Constants.SPEED * 2;
-        Matter.Body.setVelocity(player.body, { x: movment, y: 0 })
+        let movment = Constants.SPEED * 3;
+        //Move player to the left, if hit on the left side
+        if (payload.pageX < Constants.MAX_WIDTH / 2) {
+            movment *= -1;
+        }
+
+        Matter.Body.setVelocity(player.body, { x: movment, y: 0 });
     }
 
     //Claculate gravitiy stuff
