c1403115ff54527acea9c00d0aba9b51496094fe
gabriel.frischknecht <gabriel.frischknecht@hotmail.ch>
2020-10-09 13:46:22 +0200

    added fullsize rendering

--- app/src/index.js
+++ app/src/index.js
@@ -4,17 +4,10 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import SimpleGame from './simpleGame';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <SimpleGame />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render( < SimpleGame / > , document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-
