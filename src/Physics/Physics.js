e90a17dc25b46f4e92d745f6f09aa562b6c6c5ed
gabriel.frischknecht <gabriel.frischknecht@hotmail.ch>
2020-10-10 08:44:51 +0200

    added keyboard support

--- src/Physics/Physics.js
+++ src/Physics/Physics.js
@@ -4,19 +4,20 @@ import Constants from "../Constants";
 const Physics = (entities, { touches, time, input }) => {
 
     //Handle User Input
-    if (input.length > 0) {
-        console.log(input);
-    }
     const { payload } = input.find(x => x.name === "onMouseDown" || x.name === "onKeyDown") || {};
     if (payload) {
-        const player = entities["player1"];
-        let movment = Constants.SPEED * 3;
-        //Move player to the left, if hit on the left side
-        if (payload.pageX < Constants.MAX_WIDTH / 2 || payload.key === "ArrowLeft") {
-            movment *= -1;
+        //If it is a keydown event we only accept arrow left or arrow right.
+        if (payload.type !== "keydown" || (payload.key === "ArrowLeft" || payload.key === "ArrowRight")) {
+            const player = entities["player1"];
+            let movment = Constants.SPEED * 3;
+            //Move player to the left, if hit on the left side
+            if (payload.pageX < Constants.MAX_WIDTH / 2 || payload.key === "ArrowLeft") {
+                movment *= -1;
+            }
+
+            Matter.Body.setVelocity(player.body, Matter.Vector.create(movment, 0));
         }
 
-        Matter.Body.setVelocity(player.body, Matter.Vector.create(movment, 0));
     }
 
     //Claculate gravitiy stuff
