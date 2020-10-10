9afc29922da6f84402bec39a24614399fef3bbaf
gabriel.frischknecht <gabriel.frischknecht@hotmail.ch>
2020-10-10 08:38:29 +0200

    added keyboard support

--- src/Physics/Physics.js
+++ src/Physics/Physics.js
@@ -4,12 +4,15 @@ import Constants from "../Constants";
 const Physics = (entities, { touches, time, input }) => {
 
     //Handle User Input
-    const { payload } = input.find(x => x.name === "onMouseDown") || {};
+    if (input.length > 0) {
+        console.log(input);
+    }
+    const { payload } = input.find(x => x.name === "onMouseDown" || x.name === "onKeyDown") || {};
     if (payload) {
         const player = entities["player1"];
         let movment = Constants.SPEED * 3;
         //Move player to the left, if hit on the left side
-        if (payload.pageX < Constants.MAX_WIDTH / 2) {
+        if (payload.pageX < Constants.MAX_WIDTH / 2 || payload.key === "ArrowLeft") {
             movment *= -1;
         }
 
