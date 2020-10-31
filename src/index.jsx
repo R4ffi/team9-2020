3222780dece41bddd75c34a42bf846dfe9507981
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2020-10-31 14:59:16 +0100

    Remove serviceWorker

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SimpleGame from './SimpleGame';
+
+ReactDOM.render(<SimpleGame />, document.getElementById('root'));
