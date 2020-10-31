c6f3b241da46324423a823e76eb3f7181bda69fe
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2020-10-31 14:15:55 +0100

    Make Ball a functional component

--- src/Entities/Ball.jsx
+++ src/Entities/Ball.jsx
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Matter from 'matter-js';
+import PropTypes from 'prop-types';
 import soccerBall from '../Assets/Images/soccer-ball.svg';
 import Constants from '../Constants';
 
-export default class Ball extends Component {
-  render() {
-    const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
-    const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
-    let x = Constants.MAX_WIDTH / 2;
-    if (this.props.body.isNotFixed) {
-      x = this.props.body.position.x;
-    }
-    const y = this.props.body.position.y - height / 2;
-    Matter.Body.setPosition(this.props.body, { x, y: this.props.body.position.y });
+const Ball = (props) => {
+  const { body } = props;
 
-    return (
-      <div
-        style={{
-          position: 'absolute',
-          width,
-          height,
-          left: x,
-          top: y,
-        }}
-      >
-        <img src={soccerBall} className="ball" alt="soccer ball" />
-      </div>
-    );
+  const width = body.bounds.max.x - body.bounds.min.x;
+  const height = body.bounds.max.y - body.bounds.min.y;
+  let x = Constants.MAX_WIDTH / 2;
+  if (body.isNotFixed) {
+    x = body.position.x;
   }
-}
+  const y = body.position.y - height / 2;
+  Matter.Body.setPosition(body, { x, y: body.position.y });
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        width,
+        height,
+        left: x,
+        top: y,
+      }}
+    >
+      <img src={soccerBall} className="ball" alt="soccer ball" />
+    </div>
+  );
+};
+
+Ball.propTypes = {
+  body: PropTypes.instanceOf(Matter.Body).isRequired,
+};
+
+export default Ball;
