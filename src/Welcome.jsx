b311042f67c3252efd523345d9122de33eef1a85
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2020-11-07 00:40:47 +0100

    Create generic component for button screens

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,45 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import ButtonScreen from './ButtonScreen';
 import welcomeImage from './Assets/Images/welcome.svg';
-import { getAbsolutWidthPosition, getAbsolutHeightPosition } from './Constants/gameWorld';
-
-const styles = {
-  button: {
-    fontSize: getAbsolutHeightPosition(3),
-    width: '100%',
-    height: '100%',
-    fontFamily: '"04b_19", "Courier New"',
-  },
-  wrapper: {
-    width: getAbsolutWidthPosition(100),
-    height: getAbsolutHeightPosition(90),
-    margin: '0 auto',
-  },
-  logo: {
-    margin: 'auto',
-    position: 'relative',
-    top: '0%',
-    width: '100%',
-    height: '80%',
-  },
-  buttonContainer: {
-    margin: 'auto',
-    position: 'relative',
-    top: '0%',
-    width: '50%',
-    height: '10%',
-  },
-};
 
 const Welcome = ({ onStart }) => (
-  <div style={styles.wrapper}>
-    <img src={welcomeImage} alt="welcome" style={styles.logo} />
-    <div style={styles.buttonContainer}>
-      <button className="btn btn-outline-secondary" type="button" onClick={onStart} style={styles.button}>
-        Spiel starten
-      </button>
-    </div>
-  </div>
+  <ButtonScreen
+    buttonText="Spiel starten"
+    imageSource={welcomeImage}
+    onClick={onStart}
+  />
 );
 
 Welcome.propTypes = {
